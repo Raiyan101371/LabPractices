d79b09c56fbd98de66f83e29626f9e2c6ead616f
Raiyan Rahman <mdrezwanrahman84@gmail.com>
2025-04-17 18:39:28 +0600

    task17

---
+++ problem17/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Settings Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SettingsScreen(),
+    );
+  }
+}
+
+class SettingsScreen extends StatefulWidget {
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _isDarkMode = false;
+
+  void _toggleDarkMode(bool value) {
+    setState(() {
+      _isDarkMode = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: Center(
+        child: SwitchListTile(
+          title: Text('Enable Dark Mode'),
+          value: _isDarkMode,
+          onChanged: _toggleDarkMode,
+        ),
+      ),
+    );
+  }
+}
